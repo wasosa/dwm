888b7f2a7a358b91fc69c61b3e3dac609350c833
Walter Sosa <wasosa@oblong.com>
2016-11-28 19:18:18 -0800

    slimmed down config further

--- config.def.h
+++ config.def.h
@@ -2,41 +2,23 @@
 
 /* appearance */
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
-static const char font[]            = "-*-*-medium-*-*-*-14-*-*-*-*-*-*-*";
-static const char normbordercolor[] = "#cccccc";
-static const char normbgcolor[]     = "#cccccc";
-static const char normfgcolor[]     = "#000000";
-static const char selbordercolor[]  = "#0066ff";
-static const char selbgcolor[]      = "#0066ff";
-static const char selfgcolor[]      = "#ffffff";
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_black[]       = "#000000";
+static const char col_gray[]        = "#cccccc";
 static const char *colors[SchemeLast][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel] =  { col_gray4, col_cyan,  col_cyan  },
+       [SchemeNorm] = { col_black, col_gray,  col_black },
+       [SchemeSel] =  { col_gray,  col_black, col_black },
 };
 
 /* tagging */
 static const char *tags[] = { "J", "K", "L", ";", "U", "I", "O" };
 
-static const Rule rules[] = {
-	/* xprop(1):
-	 *	WM_CLASS(STRING) = instance, class
-	 *	WM_NAME(STRING) = title
-	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
-};
+static const Rule rules[1] = {};
 
 /* layout(s) */
 static const float mfact     = 0.5 ; /* factor of master area size [0.05..0.95] */
@@ -56,20 +38,18 @@ static const Layout layouts[] = {
         { MODKEY|ControlMask,           KEY,      view,           {.ui = 1 << TAG} }, \
         { MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} },
 
-/* helper for spawning shell commands in the pre dwm-5.0 fashion */
-#define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
-
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run",
                                   "-m", dmenumon,
                                   "-fn", dmenufont,
-                                  "-nb", col_gray1,
-                                  "-nf", col_gray3,
-                                  "-sb", col_cyan,
-                                  "-sf", col_gray4,
+                                  "-nb", col_gray,
+                                  "-nf", col_black,
+                                  "-sb", col_black,
+                                  "-sf", col_gray,
                                   NULL
                                 };
+
 static const char *termcmd[]  = { "gnome-terminal",
                                   NULL
                                 };
